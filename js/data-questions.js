243467519c3bc8f31f0782dab8b8bf5f7f7c62fc
Manuela <fmanuela499@gmail.com>
2018-02-22 23:09:18 -0500

    funcionalidad preguntas parte-1

---
+++ js/data-questions.js
@@ -0,0 +1,145 @@
+var questions = {
+  'groupOne': [
+    {
+      'Question': '¿Sabes si debes pagar a Laboratoria por la formación que recibirías en caso de ser aceptada?',
+      'Time': 45
+    },
+    {
+      'Question': '¿Sabes cuales son los pasos que siguen en el proceso de selección, posteriores a esta entrevista?',
+      'Time': 30
+    },
+    {
+      'Question': '¿Cuál es tu mayor grado de Educación?',
+      'Time': 60
+    }],
+  'groupTwo': [
+    {
+      'Question': 'Si estás estudiando o estudiaste alguna carreara en el pasado, cuéntanos qué y por qué lo elegiste. Si no estudias o estuiaste, cuéntanos por qué no.',
+      'Time': 60
+    },
+    {
+      'Question': '¿Qué papel jugaría Laboratoria en tu vida académica? ¿La ves más como un complemento a tus estudios o un cambio de tu carrera profesional?',
+      'Time': 45
+    },
+    {
+      'Question': '¿Cuáles son tus aspiraciones de carrera profesional y porqué? ¿Cómo llegarás a eso?',
+      'Time': 60
+    },
+    {
+      'Question': 'Cuéntanos de una situación difícil o retadora que hayas tenido con un equipo de trabajo (en el estudio o trabajo), cómo reaccionaste tú frente a la situación y si tomaste alguna acción para ayudar a resolver el problema',
+      'Time': 60
+    },
+    {
+      'Question': 'Basada en tu experiencia laboral o de estudios actual o pasada ¿Disfrutas más de las tareas que tienes que desempeñar en equipo o sola? ¿Por qué?',
+      'Time': 45
+    }],
+  'groupThree': [
+    {
+      'Question': '¿Cómo describirías a tu hogar, brevemente',
+      'Time': 45
+    },
+    {
+      'Question': 'Describe una decisión difícil que te haya tocado enfrentar y cómo lo resolviste. ¿Qué aprendiste de esto? ¿Qué aprendiste de ti misma?',
+      'Time': 60
+    },
+    {
+      'Question': '¿Qué cosas te gustan y cuales no de tu vida actual? Ya sea trabajo, amistades, etc. ',
+      'Time': 45
+    },
+    {
+      'Question': '¿Qué otra cosa te gustaría que sepamos de ti para considerar en tu postulación a Laboratoria?',
+      'Time': 30
+    },
+    {
+      'Question': '¿Qué habilidades esperas aprender estudiando en Laboratoria y cómo contribuirían a tu carrera profesional?',
+      'Time': 45
+    },
+    {
+      'Question': '¿En dónde y en qué rol te ves trabajando apenas te gradues? ',
+      'Time': 30
+    },
+    {
+      'Question': 'Si tuvieras una hora extra cada día qué harías con ella',
+      'Time': 30
+    },
+    {
+      'Question': '¿De qué logro estás realmente orgullosa?',
+      'Time': 45
+    },
+    {
+      'Question': '¿Qué superpoder querrías y por qué?',
+      'Time': 60
+    }]
+}
+
+// recorriendo las propiedades del objeto :
+// determinando la longitud de las propiedades : 
+
+var propertiesObjectArray = Object.keys(questions);
+var propertiesObjectLength = Object.keys(questions).length;
+
+
+// obteniendo los indices de las propiedades de los objetos en un array : 
+function getLength() {
+  var arr = [];
+  for (var i = 0; i < propertiesObjectLength; i++) {
+    // devuelve el array de cada propiedad : 
+    arr.push(questions[propertiesObjectArray[i]].length);
+
+  }
+  return arr;
+}
+
+for (var i = 0; i < propertiesObjectLength; i++) {
+  // devuelve el array de cada propiedad : 
+  console.log(questions[propertiesObjectArray[i]]);
+
+}
+
+
+
+// Define la cantidad de numeros aleatorios.
+function getQuestions(arrayIndex, totalQuestions, group) {
+  // debugger
+  var cantidadNumeros = arrayIndex;
+  var preguntasEscogidasPrueba = [];
+  var myArray = []
+  while (myArray.length < cantidadNumeros) {
+    var numeroAleatorio = Math.ceil(Math.random() * cantidadNumeros - 1);
+    var existe = false;
+    for (var i = 0; i < myArray.length; i++) {
+      if (myArray[i] == numeroAleatorio) {
+        existe = true;
+        break;
+      }
+    }
+    if (!existe) {
+      myArray[myArray.length] = numeroAleatorio;
+    }
+
+  }
+  for (var i = 0; i < myArray.length; i++) {
+    preguntasEscogidasPrueba.push(questions[propertiesObjectArray[group]][myArray[i]].Question);
+  }
+
+  return preguntasEscogidasPrueba.slice(0, totalQuestions);
+}
+
+// primera prueba : 
+var cantidadNumeros = 5;
+var myArray = []
+while (myArray.length < cantidadNumeros) {
+  var numeroAleatorio = Math.ceil(Math.random() * cantidadNumeros - 1);
+  var existe = false;
+  for (var i = 0; i < myArray.length; i++) {
+    if (myArray[i] == numeroAleatorio) {
+      existe = true;
+      break;
+    }
+  }
+  if (!existe) {
+    myArray[myArray.length] = numeroAleatorio;
+  }
+
+}
+console.log(myArray)
