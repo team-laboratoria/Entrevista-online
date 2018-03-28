cd2fa0a39955a8f8d035ee775da89afb2332a83b
Danielapariona <daniela.parionac@gmail.com>
2018-03-28 00:45:42 -0500

    Add warning modal

--- js/welcome.js
+++ js/welcome.js
@@ -1,5 +1,9 @@
 // redireccionando
-    var initButton = document.querySelector('.init-js');
-    initButton.addEventListener('click', function () {
-      window.location.href= 'views/question.html'
-    })
+var initButton = document.querySelector('.init-js');
+
+let ready = document.querySelector('#ready');
+console.log(ready);
+
+ready.addEventListener('click', function () {
+  window.location.href = '../views/question.html'
+})
